0ae0212ed4ad3ad7e5796eb4a57aa9e94b675d18
manio4444 <kalinowskixmarcin@o2.pl>
2018-04-21 00:14:16 +0200

    package lock + some css

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="pl">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="keywords" content="tapicerzy mińsk mazowiecki, zakłady tapicerskie, tapicerka, usługi tapicerskie, tapicerstwo samochodowe, tapicerowanie, tapicerstwo meblowe, mińsk mazowiecki" />
     <meta name="description" content="Renowacja mebli tapicerskich, meble tapicerowane na zamówienie, tapicerstwo samochodowe, obijanie drzwi." />
     <title>Zakład Tapicerski Arkadiusz Perzanowski</title>
@@ -10,21 +11,49 @@
   <body>
 
     <header class="navigation">
-      <div class="section_wrapper section_wrapper--navigation">
+      <div class="section__wrapper section__wrapper--navigation">
         <h1 class="navigation_logo">
           <span class="logo_txt--big">Zakład Tapicerski</span>
           <span class="logo_txt--small">Arkadiusz Perzanowski</span>
         </h1>
         <nav>
           <ul class="navigation_list">
-            <li class="navigation_list_el"><a href="#asdasasdasd">Home</a></li>
-            <li class="navigation_list_el"><a href="#asdasasdasd">O nas</a></li>
-            <li class="navigation_list_el"><a href="#asdasasdasd">Galeria prac</a></li>
-            <li class="navigation_list_el"><a href="#asdasasdasd">Kontakt</a></li>
+            <li class="navigation_list_el"><a href="#section_home">Home</a></li>
+            <li class="navigation_list_el"><a href="#section_about">O nas</a></li>
+            <li class="navigation_list_el"><a href="#section_gallery">Galeria prac</a></li>
+            <li class="navigation_list_el"><a href="#section_contact">Kontakt</a></li>
           </ul>
         </nav>
       </div>
     </header>
+
+    <main class="page__container">
+
+      <section class="page__section section_home" id="section_home">
+        <div class="section__wrapper">
+
+        </div>
+      </section>
+
+      <section class="page__section section_about" id="section_about">
+        <div class="section__wrapper">
+          Jesteśmy na rynku od 20 lat. Nasze doświadczenie pozwoli o nam na tworzenie nietypowych mebli tapicerowanych na  yczenie klienta. Zajmujeny sie renowacj  mebli tapicerskich, odnawiamy i zmieniamy obicia mebli codziennego u ytku, wykonujemy meble tapicerowane na zam wienie,  awki do si owni, meble do salon w fryzjerskich i restauracji, zajmujemy sie tapicerk  samochodow , obijaniem drzwi.
+        </div>
+      </section>
+
+      <section class="page__section section_gallery" id="section_gallery">
+        <div class="section__wrapper">
+
+        </div>
+      </section>
+
+      <section class="page__section section_contact" id="section_contact">
+        <div class="section__wrapper">
+
+        </div>
+      </section>
+
+    </main>
 
 
   </body>
